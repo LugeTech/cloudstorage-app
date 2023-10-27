765186387f6dc46acee28e459bfce6d560211fac
Clint Eastman <clint4life@gmail.com>
2023-10-26 20:31:56 -0400

    save state

--- src/app/components/FileUploadForm.tsx
+++ src/app/components/FileUploadForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState } from 'react';
+import { createBlobFromFile, writeBlobToFileSystem, writeBinaryFileToFileSystem} from '@/app/utils/FileOperations'
 
 const FileUploadForm: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -16,13 +17,23 @@ const FileUploadForm: React.FC = () => {
     setNewFileName(e.target.value);
   };
 
-  const handleUpload = () => {
-    // You can implement the file upload logic here.
-    if (file) {
-      // Rename the file
-      const renamedFile = new File([file], newFileName || file.name, {
-        type: file.type,
-      });
+  const handleUpload = async (formData:FormData) => {
+    const filename = formData.get('filename') as string
+    // console.log('handleUpload', filename)
+    const file = formData.get('file') as string
+
+    // const blob = await writeBinaryFileToFileSystem(file, filename);
+
+    const blob = await createBlobFromFile(new File([file], filename || 'test.txt'))
+    console.log('handleUpload', blob)
+    return
+    console.log('handleUpload', file)
+
+    // if (file) {
+    //   // Rename the file
+    //   const renamedFile = new File([file], newFileName || file.name, {
+    //     type: file.type,
+    //   });
 
       // Now you can upload the renamed file using an HTTP request, e.g., Axios.
       // Implement your upload logic here.
@@ -34,15 +45,18 @@ const FileUploadForm: React.FC = () => {
   };
 
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <input className='dark:text-neutral-950'
-        type="text"
-        placeholder="New File Name"
-        value={newFileName}
-        onChange={handleRenameChange}
-      />
-      <button onClick={handleUpload}>Upload</button>
+    <div className=' flex flex-row md:flex-row'>
+      <form>
+        <label htmlFor="file-upload">Upload a File</label>
+        <input name='file' id="file-upload" type="file" onChange={handleFileChange} />
+        <input name='filename' className='dark:text-neutral-950'
+          type="text"
+          placeholder="New File Name"
+          value={newFileName}
+          onChange={handleRenameChange}
+        />
+        <button type='submit' formAction={handleUpload}>Upload</button>
+      </form>
     </div>
   );
 };
