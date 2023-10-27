649156f07f60b9f277e34ddda7394e71b1ecc0de
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-27 00:10:12 -0400

    submitform route

--- src/app/api/submit-form/route.ts
+++ src/app/api/submit-form/route.ts
@@ -1,18 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { encryptBlob } from '../../utils/encrypt';
 import fs from 'fs';
 export async function POST(request: NextRequest) {
   const formData = await request.formData();
   const file = formData.get("file") as File;
+  const filename = formData.get("filename") as string;
 
   if (!file) {
-    // No file was uploaded
     return;
   }
 
   const buffer = await file.arrayBuffer();
   const dataView = new DataView(buffer);
 
-  fs.writeFileSync("./uploads/" + file.name, dataView);
+  fs.writeFileSync(filename, dataView);
   // Process the form data
 
   return NextResponse.json({ success: true });
