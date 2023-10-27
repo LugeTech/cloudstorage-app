5f99d418ed299bbf5c00f4e3f795426e93d52873
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-27 18:22:29 -0400

    add time code to the end of username for file name

--- src/app/api/submit-form/route.ts
+++ src/app/api/submit-form/route.ts
@@ -1,10 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { encryptBlob } from '../../utils/encrypt';
 import fs from 'fs';
+const unixTime = Date.now();
+
 export async function POST(request: NextRequest) {
   const formData = await request.formData();
   const file = formData.get("file") as File;
-  const filename = formData.get("filename") as string || "default";
+  const filename = formData.get("filename") as string || "userName";
 
   if (!file) {
     console.log("No file found");
@@ -15,8 +17,13 @@ export async function POST(request: NextRequest) {
   const buffer = await file.arrayBuffer();
   const dataView = new DataView(buffer);
 
-  fs.writeFileSync(`./public/uploads/${filename}`, dataView);
-  // Process the form data
+  const newFilename = `${filename}_${unixTime}`;
+
+  try {
+    fs.writeFileSync(`./public/uploads/${newFilename}`, dataView);
+  } catch (error) {
+    // Handle the error
+  }  // Process the form data
 
   return NextResponse.json({ success: true });
 }
