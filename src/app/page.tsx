fbf61126da4114d19616b7c1adced1c09b7fd31c
Clint Eastman <clint4life@gmail.com>
2023-10-27 20:07:34 -0400

    Added user profile capabilites. Along with sign-in/sign-out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
+"use client";
 import DragAndDrop from './components/DragAndDrop'
+import { UserButton, useUser } from "@clerk/nextjs";
 import FileUploadForm from './components/FileUploadForm'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      {<h1 className="text-4xl font-bold text-center">Cloud App</h1>}
-      {/* <div><FileUploadForm /></div> */}
-      <DragAndDrop />
-
-    </main>
-  )
+  const { isLoaded, isSignedIn } = useUser();
+  if (!isLoaded || !isSignedIn) {
+    return (
+      
+      <button onClick={() => window.location.href = '/sign-in'}>Sign In</button>
+    );
+  }
+  else{
+    return (
+      <main className="flex min-h-screen flex-col items-center p-24">
+        {<h1 className="text-4xl font-bold text-center">Cloud App</h1>}
+        {/* <div><FileUploadForm /></div> */}
+        <DragAndDrop />
+        <UserButton afterSignOutUrl="/"/>
+      </main>
+    )
+  }
 }
