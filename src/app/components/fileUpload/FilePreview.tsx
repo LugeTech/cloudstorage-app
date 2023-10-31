560b49a28a71a4abb63e68895567593afe1628de
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-31 03:08:25 -0400

    minor ui tweak

--- src/app/components/fileUpload/FilePreview.tsx
+++ src/app/components/fileUpload/FilePreview.tsx
@@ -8,16 +8,16 @@ type Props = {
 }
 const AFileDisplay = ({ imagePreviews, files, uploadProgress, removeFile }: Props) => {
   return (
-    <div className="flex  w-full h-full p-4 gap-2 flex-wrap justify-center items-center bg-gray-100 rounded ">
+    <div className="flex  w-full h-full p-4 gap-2 flex-wrap justify-center items-center ">
       {imagePreviews.map((preview, index) => (
-        <div key={index} className="">
+        <div key={index} className="flex flex-col items-center">
           <Image src={preview} alt={`Image ${index}`} width={100} height={100} className="object-cover w-24 h-24" />
           <p className="mt-2 text-gray-600 text-sm text-center">
             {files[index].name.length > 10
               ? files[index].name.substring(0, 10) + '...' // If the name is longer than 20 characters, add ellipsis.
               : files[index].name}
           </p>
-          {uploadProgress[index] > 0 && <ProgressBar completed={uploadProgress[index]} className="mt-1 w-24 px-4" bgColor="#4caf50" height="12px" labelSize='12px' />}
+          {uploadProgress[index] > 0 && <ProgressBar completed={uploadProgress[index]} className="my-1 w-24 px-2" bgColor="#4caf50" height="12px" labelSize='12px' />}
           <button
             onClick={() => removeFile(index)}
             className=" rounded-md bg-red-500 px-2 py-1 text-white"
