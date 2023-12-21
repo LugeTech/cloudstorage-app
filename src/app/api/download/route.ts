d68d6d5d49bf910aa3b1bfd3cfaf2cd18d0b7eb1
Ken Taylor <kentaylosappdev@gmail.com>
2023-12-21 06:15:59 -0400

    forgot to switch the the decription import

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -1,24 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 import fs from "fs";
-import { decryptBuffer } from "@/app/utils/encryptAndDecrypt";
+import { decryptBuffer } from "@/app/utils/decryption";
 
 const unixTime = Date.now();
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
   console.log("Download endpoint");
+  const filePath: string = "./public/uploads/christena.jpg";
 
+  function readFileToBuffer(filePath: string): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
   //get user info and cross refence with database to get file info
   // make sure file exists and maybe load first chunk with fs and create buffer
   // get all details from the frontend sent in clerk token and this formdata
-  const formData = await request.formData();
+  // const formData = await request.formData();
 
   try {
     // pass the encrypted data through the decryptbuffer function
     // not 100% sure but maybe we save this decrypted file to a temp area to create a download link that will be deleted after the file is downloaded
+    const fileAsBuffer = await readFileToBuffer(filePath);
+
+    const decryptedBuffer = await decryptBuffer(fileAsBuffer);
+    fs.writeFileSync(`./public/uploads/file.jpg`, decryptedBuffer);
   } catch (error) {
     // Handle the error
     console.error(error);
-    return NextResponse.json({ success: false });
+    return NextResponse.json({ success: false, error: error });
   }
 
   // Process the form data
